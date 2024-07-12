95b2faf1f5ae5ff9f57563a1696b94dacdb1e2e6
josue173 <josue.perez04@outlook.com>
2024-07-11 19:30:03 -0600

    Estrategia e interfaz

---
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -0,0 +1,8 @@
+export interface JwtPayload {
+  id:string;
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  date_of_birthday: Date;
+}
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from '../entities/user.entity';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly _userRepository: Repository<User>,
+    configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { email } = payload;
+    const user = await this._userRepository.findOneBy({ email });
+    if (!user) throw new UnauthorizedException('Token not valid');
+    if (!user.is_active) throw new UnauthorizedException('User is inactive');
+    return user;
+    // El retorno de 'user' se agrega a la request 
+  }
+}
